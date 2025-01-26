fe13a49003b692067e34d2d6e6775ded483dde2d
MichaelGerrardStanleyH <hutagalung.michael23@gmail.com>
2025-01-26 14:21:16 +0700

    initia commit

---
+++ src/main/java/com/parkee/parkee_backend_engineer_test/ParkeeBackendEngineerTestApplication.java
@@ -0,0 +1,13 @@
+package com.parkee.parkee_backend_engineer_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkeeBackendEngineerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkeeBackendEngineerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkee/parkee_backend_engineer_test/ParkeeBackendEngineerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkee.parkee_backend_engineer_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkeeBackendEngineerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
